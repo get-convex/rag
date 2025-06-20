7bfdc2505275979e1a091da2b7a24964ee781c3f
Ian Macartney <ianhmacartney@gmail.com>
2025-06-20 16:41:15 -0700

    importance tested and working

--- src/component/embeddings/importance.ts
+++ src/component/embeddings/importance.ts
@@ -16,39 +16,46 @@ export function searchVector(embedding: number[]) {
 }
 /**
  * For an importance of x (0 to 1):
-
  * @param embedding - The vector to modify with an importance weight.
  * @param importance - 0 - 1, where 0 is no importance and 1 is full importance.
  * @returns The vector with the importance added.
  */
-
 export function vectorWithImportance(embedding: number[], importance: number) {
   /*
    * Goal: add a weighting that reduces the magnitude of the target vector after
    * normalization.
-   * 1. Normalize the existing vector to (1-x) and add √.x
+   * 1. Scale the existing vector by importance and add a weight so |v| = 1.
    * 2. Search with [...embedding, 0].
    * e.g.:
    * Say we have an embedding of 2 numbers [.6, .8]
-   * For 50% importance: [.3, .4, .707]
+   * For 50% importance: [.6 * i, .8 * i, sqrt(1 - i^2)] ->  [.3, .4, .866]
    * For [.6, .8] we used to get 1.0.
-   * Now we get .6*.3 + .8+.4+0 = .5
+   * Now we get .6*.3 + .8*.4 + 0*.866 = 0.5
    */
   // We drop the final dimension if it'd make it larger than 4096.
   // Unfortunate current limitation of Convex vector search.
-  const vectorToModify =
-    embedding.length === 4096 ? embedding.slice(0, 4095) : embedding;
-  const normalized = normalizeVector(vectorToModify);
+  const vectorToModify = normalizeVector(
+    embedding.length === 4096 ? embedding.slice(0, 4095) : embedding
+  );
+  const scaled = scaleVector(vectorToModify, importance);
 
-  const sqrtImportance = Math.sqrt(importance);
-  return [...normalized, sqrtImportance];
+  // |embedding| == 1
+  // weight^2 + importance^2(|embedding|^2) == 1
+  // weight = sqrt(1 - importance^2)
+  const weight = Math.sqrt(1 - importance ** 2);
+  return [...scaled, weight];
 }
+
 function normalizeVector(vector: number[]) {
   const sumOfSquares = vector.reduce((acc, v) => acc + v * v, 0);
   const magnitude = Math.sqrt(sumOfSquares);
   return magnitude === 0
     ? vector.map(() => 0)
     : vector.map((v) => v / magnitude);
+}
+
+function scaleVector(vector: number[], scale: number) {
+  return vector.map((v) => v * scale);
 }
 
 export function modifyImportance(vector: number[], importance: number) {
@@ -59,7 +66,7 @@ export function modifyImportance(vector: number[], importance: number) {
 }
 
 export function getImportance(vector: number[]) {
-  return vector[vector.length - 1] ** 2;
+  return Math.sqrt(1 - vector[vector.length - 1] ** 2);
 }
 
 export function vectorWithImportanceDimension(dimensions: number) {
