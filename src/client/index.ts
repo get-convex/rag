9ef619283f88cd70a6a0471b247af5df76462e90
Ian Macartney <ianhmacartney@gmail.com>
2025-06-27 14:36:57 -0700

    add hybrid rank function

--- src/client/index.ts
+++ src/client/index.ts
@@ -579,24 +579,64 @@ async function createChunkArgsBatch(
   }) as CreateChunkArgs[];
 }
 
-  async function getSource(
-    ctx: ActionCtx,
-    {
-      storageId,
-      url,
-      contentHash,
-    }: { storageId?: string; url?: string; contentHash?: string }
-  ): Promise<{ source: Source; contentHash: string }> {
-    assert(storageId || url, "Either storageId or url must be provided");
-    if (storageId) {
-      const metadata = await ctx.storage.getMetadata(storageId);
-      assert(metadata, "Storage metadata not found for storageId " + storageId);
-      return {
-        source: { kind: "_storage", storageId },
-        contentHash: metadata.sha256,
-      };
-    } else {
-      assert(url);
-      return { source: { kind: "url", url }, contentHash: contentHash ?? url };
+async function getSource(
+  ctx: ActionCtx,
+  {
+    storageId,
+    url,
+    contentHash,
+  }: { storageId?: string; url?: string; contentHash?: string }
+): Promise<{ source: Source; contentHash: string }> {
+  assert(storageId || url, "Either storageId or url must be provided");
+  if (storageId) {
+    const metadata = await ctx.storage.getMetadata(storageId);
+    assert(metadata, "Storage metadata not found for storageId " + storageId);
+    return {
+      source: { kind: "_storage", storageId },
+      contentHash: metadata.sha256,
+    };
+  } else {
+    assert(url);
+    return { source: { kind: "url", url }, contentHash: contentHash ?? url };
+  }
+}
+
+/**
+ * Rank results from multiple results, e.g. from vector search and text search.
+ * Uses the "Recriprocal Rank Fusion" algorithm.
+ * @param sortedResults The results arrays ordered by most important first.
+ */
+export function hybridRank<T extends string>(
+  sortedResults: T[][],
+  opts?: {
+    /**
+     * A constant used to change the bias of the top results in each list vs.
+     * results in the middle of multiple lists.
+     * A higher k means less of a bias toward the top few results.
+     */
+    k: number;
+    /**
+     * The weights of each sortedResults array.
+     * Used to prefer results from one sortedResults array over another.
+     */
+    weights: number[];
+    /**
+     * The cutoff score for a result to be returned.
+     */
+    cutoffScore?: number;
+  }
+): T[] {
+  const k = opts?.k ?? 10;
+  const scores: Map<T, number> = new Map();
+  for (const [i, results] of sortedResults.entries()) {
+    const weight = opts?.weights?.[i] ?? 1;
+    for (let j = 0; j < results.length; j++) {
+      const key = results[j];
+      scores.set(key, (scores.get(key) ?? 0) + weight / (k + j));
     }
   }
+  const sortedScores = Array.from(scores.entries()).sort((a, b) => b[1] - a[1]);
+  return sortedScores
+    .filter(([_, score]) => score >= (opts?.cutoffScore ?? 0))
+    .map(([key]) => key);
+}
