1ab750d6776f3141302c47cf23e437a9d8588b5b
Ian Macartney <ianhmacartney@gmail.com>
2025-10-15 22:10:06 -0700

    better test

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,11 @@
-import { TestConvex } from "convex-test";
+import type { TestConvex } from "convex-test";
+import type { GenericSchema, SchemaDefinition } from "convex/server";
 import schema from "./component/schema.js";
 const modules = import.meta.glob("./component/**/*.ts");
-function register(t: TestConvex<any>, name: string) {
+function register(
+  t: TestConvex<SchemaDefinition<GenericSchema, boolean>>,
+  name: string
+) {
   t.registerComponent(name, schema, modules);
 }
 export default { schema, modules, register };
