8f4bb62559f91d81498841213ea90ccbc167d6cd
Ian Macartney <ianhmacartney@gmail.com>
2025-11-04 01:43:35 -0800

    add test config

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,11 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envDir: "../",
   plugins: [react()],
   optimizeDeps: {
-    include: ["pdfjs-dist"],
-  },
-  resolve: {
-    conditions: ["@convex-dev/component-source"],
-  },
+    include: ["pdfjs-dist"]
+  }
 });
