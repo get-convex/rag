28eabd3d8891f93ad979b131933d0bdca1c177dc
Ian Macartney <ianhmacartney@gmail.com>
2025-11-06 00:41:30 -0800

    fix client test setup

--- src/component/setup.test.ts
+++ src/component/setup.test.ts
@@ -3,17 +3,11 @@ import { test } from "vitest";
 import { convexTest } from "convex-test";
 import schema from "./schema.js";
 export const modules = import.meta.glob("./**/*.*s");
-
-// Sorry about everything
-import componentSchema from "../../node_modules/@convex-dev/workpool/src/component/schema.js";
-export { componentSchema };
-export const componentModules = import.meta.glob(
-  "../../node_modules/@convex-dev/workpool/src/component/**/*.ts",
-);
+import workpool from "@convex-dev/workpool/test";
 
 export function initConvexTest() {
   const t = convexTest(schema, modules);
-  t.registerComponent("workpool", componentSchema, componentModules);
+  t.registerComponent("workpool", workpool.schema, workpool.modules);
   return t;
 }
 
