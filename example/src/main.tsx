2a23859ebb2437e56433411558da303ee25de306
Ian Macartney <ianhmacartney@gmail.com>
2025-11-06 00:42:25 -0800

    lint

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import Example from "./Example.tsx";
+import Example from "./Example.jsx";
 import "./index.css";
 
 const address = import.meta.env.VITE_CONVEX_URL;
